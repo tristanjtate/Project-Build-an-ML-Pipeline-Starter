bd525b1fb729176053e3388f7cf6594266b032a6
Tristan <Tristan>
2025-09-24 10:07:48 -0700

    add geolocation filter to remove out of bounds rows

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,8 +30,12 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+
+    # Remove listings that are not in the proper geolocation
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+
     df = df[idx].copy()
+    
     # Save the cleaned file
     df.to_csv('clean_sample.csv',index=False)
 
